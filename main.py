3867ee66809c7753a8eb5cb95e8a8c96c727e8a8
WhyAsh5114 <whyash5114@gmail.com>
2022-06-08 18:42:45 +0530

    some cleanup

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import dota2api
 import requests
 import webbrowser
 
@@ -7,11 +6,13 @@ from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.button import Button
 from kivy.core.window import Window
+from kivy.clock import Clock
 from time import sleep
 from threading import Thread
 
 
 class MainScreen(Screen):
+
 
     def find_matches(self):
         self.ids['find'].disabled = True
@@ -19,83 +20,74 @@ class MainScreen(Screen):
         t1 = Thread(target=self.add_matches, daemon=True)
         t1.start()
 
+
     def add_matches(self):
         API_KEY = self.ids['api_key'].text
         ACCOUNT_ID = int(self.ids['account_id'].text)
-        BASE_TIMEOUT = 1
-        NUMBER_OF_MATCHES = int(self.ids['number_of_matches'].text)
-        api = dota2api.Initialise(API_KEY)
-        hero_data = requests.get("https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/?key={}".format(API_KEY)).json()
-        if hero_data['result']['status'] == 200:
-            total_matches = 0
-            matches = []
-            self.ids['progress'].max = hero_data['result']['count']
-            i = 0
-            for hero in hero_data['result']['heroes']:
-                self.ids['find'].text = "Queuing matches of {}: {}/{}".format(hero['name'].replace("npc_dota_hero_", ""), i+1, hero_data['result']['count'])
-                hero_matches = []
-                for match in api.get_match_history(matches_requested=NUMBER_OF_MATCHES, account_id=ACCOUNT_ID, hero_id=hero['id'])['matches']:
-                    hero_matches.append(match["match_id"])
-                    total_matches += 1
-                sleep(BASE_TIMEOUT)
-                i += 1
-                self.ids['progress'].value = i
-                matches.append(hero_matches)
-            self.ids['progress'].value = 1
-            self.ids['progress'].max = total_matches
-            print(total_matches)
-            self.loop_matches(matches, ACCOUNT_ID)
-        else:
-            self.ids['find'].text = "Server Error"
 
-    def loop_matches(self, recent_matches, ACCOUNT_ID):
-        for hero_matches in recent_matches:
-            parse = True
-            for match in hero_matches:
-                print(match)
-                self.ids['progress'].value += 1
-                self.ids['find'].text = "Scanning match (hero{}): {}/{}".format(str(recent_matches.index(hero_matches)+1), str(int(self.ids['progress'].value)), self.ids['progress'].max)
-                if not self.check_match(match, ACCOUNT_ID, parse):
-                    parse = False
+        req_num = int(self.ids['number_of_matches'].text)
+        NUMBER_OF_MATCHES = req_num if req_num < 100 else 100
+        
+        hero_data = requests.get("https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/?key={}".format(API_KEY)).json()
+        matches = []
+        self.ids['progress'].max = hero_data['result']['count']
+        raw_matches = requests.get(f"https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?account_id={ACCOUNT_ID}&matches_requested={NUMBER_OF_MATCHES}&key={API_KEY}").json()['result']['matches']
+        for match in raw_matches:
+            matches.append(match["match_id"])
+
+        self.ids['progress'].max = NUMBER_OF_MATCHES
+        self.loop_matches(matches, ACCOUNT_ID, NUMBER_OF_MATCHES)
+
+
+    def loop_matches(self, recent_matches, ACCOUNT_ID, number_of_matches):
+        for i, match in enumerate(recent_matches, 1):
+            self.ids['progress'].value = i
+            self.ids['find'].text = f"Scanning match: {str(i)}/{number_of_matches}"
+            self.check_match(match, ACCOUNT_ID, i, number_of_matches)
         self.ids['find'].text = "Done"
 
 
-    def check_match(self, match, ACCOUNT_ID, parse):
+    def check_match(self, match, ACCOUNT_ID, i, number_of_matches):
         BASE_TIMEOUT = 1
         PARSE_TIMEOUT = 10
         BASE_API = "http://api.opendota.com/api/matches/"
+
         match_details = requests.get(BASE_API + str(match)).json()
-        if parse == True:
-            if match_details['version'] is None:
-                self.ids['find'].text = "Requesting parse of match ({}): {}/{}".format(self.ids['find'].text.split("(")[1].split(")")[0], self.ids['find'].text.split(" ")[-1].split("/")[0], self.ids['find'].text.split(" ")[-1].split("/")[1])
-                try:
-                    requests.post("https://api.opendota.com/api/request/" + str(match))
-                    wait = 0
-                    while match_details['version'] is None:
-                        self.ids['find'].text = "Parsing match ({}): {}/{}".format(self.ids['find'].text.split("(")[1].split(")")[0], self.ids['find'].text.split(" ")[-1].split("/")[0], self.ids['find'].text.split(" ")[-1].split("/")[1])
-                        sleep(PARSE_TIMEOUT)
-                        match_details = requests.get(BASE_API + str(match)).json()
-                        wait += 10
-                        if wait > 60:
-                            self.ids['find'].text = "API Timeout, Trying next... (Maybe API call limit reached)"
-                            sleep(2)
-                            break
-                    else:
-                        self.ids['find'].text = "Replay not available, skipping parsing"
-                        sleep(BASE_TIMEOUT)
-                        return False
-                except Exception as e:
-                    print(e)
-        if match_details['version'] is not None:
-            for player in match_details['players']:
-                if player["account_id"] == ACCOUNT_ID:
-                    if player['multi_kills'] is not None and '5' in player['multi_kills'].keys():
-                        link_btn = Button(text="http://www.opendota.com/matches/" + str(match))
-                        link_btn.bind(on_release=lambda touch: webbrowser.open(link_btn.text))
-                        self.ids['links'].add_widget(link_btn)
+        if match_details['version'] is None:
+            self.ids['find'].text = f"Requesting parse of match: {i}/{number_of_matches}"
+            try:
+                requests.post(f"https://api.opendota.com/api/request/{str(match)}")
+                wait = 0
+                while match_details['version'] is None:
+                    self.ids['find'].text = f"Parsing match: {i}/{number_of_matches}"
+                    wait += PARSE_TIMEOUT
+                    sleep(PARSE_TIMEOUT)
+                    match_details = requests.get(BASE_API + str(match)).json()
+                    if wait > 60:
+                        self.ids['find'].text = "API Timeout, Trying next... (Maybe API call limit reached)"
+                        sleep(2)
                         break
-            sleep(BASE_TIMEOUT)
+                if match_details['version'] is None:
+                    self.ids['find'].text = "Replay not available, skipping parsing"
+                    sleep(BASE_TIMEOUT)
+                    return False
+            except Exception as e:
+                print(e)
+
+        for player in match_details['players']:
+            if player["account_id"] == ACCOUNT_ID:
+                if player['multi_kills'] is not None and '5' in player['multi_kills'].keys():
+                    Clock.schedule_once(lambda dt: self.add_link("http://www.opendota.com/matches/" + str(match)))
+                    print(f"Rampage found in match: {match}")
+        sleep(BASE_TIMEOUT)
         return True
+
+
+    def add_link(self, txt):
+        link_btn = Button(text=txt)
+        link_btn.height = 10
+        link_btn.bind(on_release=lambda touch: webbrowser.open(link_btn.text))
+        self.ids['links'].add_widget(link_btn)
 
 
 class WindowManager(ScreenManager):
